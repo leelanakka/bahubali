ce5fb0ba457216b655e46ca13a870dd36172c693
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-12-29 16:55:22 +0530

    modifies install of npm module in globally

--- getUserCommits.js
+++ getUserCommits.js
@@ -3,8 +3,12 @@ const shelljs = require("shelljs");
 const fs = require("fs");
 const userNames = fs.readFileSync("./userNames.txt", "utf8").split("\n");
 userNames.pop();
+
 let a = shelljs.ls().stdout.split("\n");
 if (!a.includes("interns")) {
+  shelljs.exec(
+    "npm install -g https://github.com/craftybones/mocha-json-reporter.git"
+  );
   shelljs.mkdir("interns");
 }
 shelljs.cd("./interns");
@@ -16,9 +20,6 @@ userNames.map(x => {
     let y = "git clone https://github.com/STEP-tw/" + x + ".git";
     shelljs.exec(y);
     shelljs.cd(x);
-    shelljs.exec(
-      "npm install https://github.com/craftybones/mocha-json-reporter.git"
-    );
     shelljs.cd("../");
     return;
   }
