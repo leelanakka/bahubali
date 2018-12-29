4cb4dd5ef7dd40c413e897ca18db1080e2e9dcdb
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-12-29 14:13:22 +0530

    it will install mocha reporter package in every repo
    it will only install for first time of cloning

--- getUserCommits.js
+++ getUserCommits.js
@@ -3,9 +3,9 @@ const shelljs = require("shelljs");
 const fs = require("fs");
 const userNames = fs.readFileSync("./userNames.txt", "utf8").split("\n");
 userNames.pop();
-let a = shelljs.ls().stdout.split('\n');
-if(!a.includes('interns')){
-  shelljs.mkdir('interns');
+let a = shelljs.ls().stdout.split("\n");
+if (!a.includes("interns")) {
+  shelljs.mkdir("interns");
 }
 shelljs.cd("./interns");
 repos = shelljs.ls().stdout.split("\n");
@@ -13,12 +13,17 @@ repos.pop();
 userNames.map(x => {
   x = "wc-" + x;
   if (!repos.includes(x)) {
-    x = "git clone https://github.com/STEP-tw/" + x + ".git";
-    shelljs.exec(x);
+    let y = "git clone https://github.com/STEP-tw/" + x + ".git";
+    shelljs.exec(y);
+    shelljs.cd(x);
+    shelljs.exec(
+      "npm install https://github.com/craftybones/mocha-json-reporter.git"
+    );
+    shelljs.cd("../");
     return;
   }
   shelljs.cd(x);
-  console.log(x)
+  console.log(x);
   shelljs.exec("git pull");
   shelljs.cd("../");
 });
