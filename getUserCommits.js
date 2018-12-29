1ae13fe0af7994137a9a68fbf19e7952e2cf7c45
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-12-29 12:21:36 +0530

    added feature of cloning the repos
    is the repos are already exists then it will pull

---
+++ getUserCommits.js
@@ -0,0 +1,24 @@
+const shelljs = require("shelljs");
+
+const fs = require("fs");
+const userNames = fs.readFileSync("./userNames.txt", "utf8").split("\n");
+userNames.pop();
+let a = shelljs.ls().stdout.split('\n');
+if(!a.includes('interns')){
+  shelljs.mkdir('interns');
+}
+shelljs.cd("./interns");
+repos = shelljs.ls().stdout.split("\n");
+repos.pop();
+userNames.map(x => {
+  x = "wc-" + x;
+  if (!repos.includes(x)) {
+    x = "git clone https://github.com/STEP-tw/" + x + ".git";
+    shelljs.exec(x);
+    return;
+  }
+  shelljs.cd(x);
+  shelljs.exec("git pull");
+  shelljs.cd("../");
+});
+shelljs.cd("../");
