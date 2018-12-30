ca7ceeb2fbdda91adfffde43d34f4010f96f1c57
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-12-30 22:50:49 +0530

    added some more test case files
    added some files to test the interns wc

--- createReport.js
+++ createReport.js
@@ -1,28 +1,28 @@
-const fs = require('fs');
-const Table = require('cli-table');
-let data = fs.readFileSync('./report.json','utf8');
+const fs = require("fs");
+const Table = require("cli-table");
+let data = fs.readFileSync("./report.json", "utf8");
 data = JSON.parse(data);
 const head = [
-    "UserName",
-    "ShaId",
-    "NoOfCommits",
-    "MochaTests",
-    "Coverage",
-    "BahubaliTests"
-]
-const table = new Table({head});
+  "UserName",
+  "ShaId",
+  "NoOfCommits",
+  "MochaTests",
+  "Coverage",
+  "BahubaliTests"
+];
+const table = new Table({ head });
 
-data.map((user) =>{
-    let MochaTests = user.MochaTestsTotal+" / "+user.MochaTestsPassed;
-    let BahubaliTests = user.TotalTests+" / "+user.TestsPassed;
-    table.push([
-        user.userName,
-        user.shaID,
-        user.noOfCommits,
-        MochaTests,
-        user.coverage,
-        BahubaliTests
-    ])
+data.map(user => {
+  let MochaTests = user.MochaTestsPassed + " / " + user.MochaTestsTotal;
+  let BahubaliTests = user.TestsPassed + " / " + user.TotalTests;
+  table.push([
+    user.userName,
+    user.shaID,
+    user.noOfCommits,
+    MochaTests,
+    user.coverage,
+    BahubaliTests
+  ]);
 });
 
 console.log(table.toString());
--- test/myTest.js
+++ test/myTest.js
@@ -10,60 +10,60 @@ const filtering = function(content) {
 };
 
 describe("wc", function() {
-  it("should return return no of lines along with file name", function() {
+  it("should return return number of lines along with file name for -l option", function() {
     let actualOut = shelljs.exec("node ./wc.js -l file");
     let actual = filtering(actualOut.stdout);
     let expected = "1 file";
     assert.deepEqual(actual, expected);
   });
 
-  it("should return no of characters along with the file name", function() {
+  it("should return byte count  along with the file name for -c option", function() {
     let actualOut = shelljs.exec("node ./wc.js -c file");
     let actual = filtering(actualOut.stdout);
     let expected = "4 file";
     assert.deepEqual(actual, expected);
   });
 
-  it("should return no of characters along with the file name", function() {
+  it("should return byte count  along with the file name for the file that dont have new line at end", function() {
     let actualOut = shelljs.exec("node ./wc.js -c file1");
     let actual = filtering(actualOut.stdout);
     let expected = "4 file1";
     assert.deepEqual(actual, expected);
   });
 
-  it("should return no of characters along with the file name", function() {
+  it("should return byte count along with the file name for multiple -c options", function() {
     let actualOut = shelljs.exec("node ./wc.js -c -c -c -c -c file1");
     let actual = filtering(actualOut.stdout);
     let expected = "4 file1";
     assert.deepEqual(actual, expected);
   });
 
-  it("should return no of characters along with the file name", function() {
+  it("should return byte and word count for -cw option", function() {
     let actualOut = shelljs.exec("node ./wc.js -ccw file1");
     let actual = filtering(actualOut.stdout);
     let expected = "1 4 file1";
     assert.deepEqual(actual, expected);
   });
-  it("should return no of characters along with the file name", function() {
+  it("should return line count as 0 and byte and word count should return for single line file", function() {
     let actualOut = shelljs.exec("node ./wc.js foo");
     let actual = filtering(actualOut.stdout);
     let expected = "0 2 13 foo";
     assert.deepEqual(actual, expected);
   });
-  it("should return no of characters along with the file name", function() {
+  it("should return all types of counts for file containing empty lines in middle if file", function() {
     let actualOut = shelljs.exec("node ./wc.js foo1");
     let actual = filtering(actualOut.stdout);
     let expected = "4 2 14 foo1";
     assert.deepEqual(actual, expected);
   });
-  it("should return no of characters along with the file name", function() {
+  it("should return all counts for multi line file and now new line at end", function() {
     let actualOut = shelljs.exec("node ./wc.js foo2");
     let actual = filtering(actualOut.stdout);
     let expected = "2 3 16 foo2";
     assert.deepEqual(actual, expected);
   });
 
-  it("should return no of characters along with the file name", function() {
+  it("should return all counts for their own main file", function() {
     let actualOut = shelljs.exec("node ./wc.js wc.js");
     let actual = filtering(actualOut.stdout);
     let expectedOut = shelljs.exec("wc wc.js");
@@ -71,7 +71,7 @@ describe("wc", function() {
     assert.deepEqual(actual, expected);
   });
 
-  it("should return no of characters along with the file name", function() {
+  it("should return all counts for nestes options like -cccclccwlc option", function() {
     let actualOut = shelljs.exec("node ./wc.js -ccclccwwlc wc.js");
     let actual = filtering(actualOut.stdout);
     let expectedOut = shelljs.exec("wc -ccclccwwlc wc.js");
@@ -79,7 +79,7 @@ describe("wc", function() {
     assert.deepEqual(actual, expected);
   });
 
-  it("should return no of characters along with the file name", function() {
+  it("should return word count for fileNames containig - in middle", function() {
     let actualOut = shelljs.exec("node ./wc.js -w foo-bar");
     let actual = filtering(actualOut.stdout);
     let expectedOut = shelljs.exec("wc -w foo-bar");
@@ -87,10 +87,42 @@ describe("wc", function() {
     assert.deepEqual(actual, expected);
   });
 
-  it("should return no of characters along with the file name", function() {
+  it("should return word  for fileNames containig - in between", function() {
     let actualOut = shelljs.exec("node ./wc.js -w foo-bar-foo");
     let actual = filtering(actualOut.stdout);
     let expectedOut = shelljs.exec("wc -w foo-bar-foo");
+    let expected = filtering(expectedOut);
+    assert.deepEqual(actual, expected);
+  });
+
+  it("should return 0 as word count for file with few empty lines", function() {
+    let actualOut = shelljs.exec("node ./wc.js -w empty");
+    let actual = filtering(actualOut.stdout);
+    let expectedOut = shelljs.exec("wc -w empty");
+    let expected = filtering(expectedOut);
+    assert.deepEqual(actual, expected);
+  });
+
+  it("should return no of characters along with the file name", function() {
+    let actualOut = shelljs.exec("node ./wc.js -w bahubali");
+    let actual = filtering(actualOut.stdout);
+    let expectedOut = shelljs.exec("wc -w bahubali");
+    let expected = filtering(expectedOut);
+    assert.deepEqual(actual, expected);
+  });
+
+  it("should return all counts as 0 for file that contains nothing", function() {
+    let actualOut = shelljs.exec("node ./wc.js -w wc.js foo-bar");
+    let actual = filtering(actualOut.stdout);
+    let expectedOut = shelljs.exec("wc -w wc.js foo-bar");
+    let expected = filtering(expectedOut);
+    assert.deepEqual(actual, expected);
+  });
+
+  it("should return word count for multiple files", function() {
+    let actualOut = shelljs.exec("node ./wc.js -w foo2 foo1");
+    let actual = filtering(actualOut.stdout);
+    let expectedOut = shelljs.exec("wc -w foo2 foo1");
     let expected = filtering(expectedOut);
     assert.deepEqual(actual, expected);
   });
